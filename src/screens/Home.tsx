5ccfb32783c92a6728433a04896ccf00bb6c829c
diegorodrigues-cubos <117923905+diegorodrigues-cubos@users.noreply.github.com>
2022-11-29 13:56:14 -0300

    add new home view

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,59 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { HStack, VStack, FlatList, Heading, Text, Stack } from "native-base";
+
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { ExerciseCard } from "@components/ExerciseCard";
 
 export function Home() {
+  const [groups, setGroups] = useState(["Costa", "Biceps", "Triceps", "Ombro"]);
+  const [groupSelected, setGroupSelected] = useState("costa");
+  const [exercises, setExercises] = useState([
+    "Puxada frontal",
+    "Remada Curvada",
+    "Remada unilateral",
+    "Levantamento terras",
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            onPress={() => setGroupSelected(item)}
+            name={item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+      <Stack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </Stack>
+    </VStack>
   );
 }
